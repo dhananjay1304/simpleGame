3444d2c296de7a2f3936f89280859f448c1b3d1a
dhananjay1304 <dhananjaypotinda2003@gmail.com>
2023-08-12 13:01:00 +0530

    anonymous

--- file.js
+++ file.js
@@ -1,8 +1,8 @@
 const startGameBtn = document.getElementById('start-game-btn');
 
-const start = function startGame() {
-    console.log('Game is starting...!');
-}
+// const start = function() {
+//     console.log('Game is starting...!');
+// };
 
 // const person = {
 //     name:'Max',
@@ -17,4 +17,6 @@ const start = function startGame() {
 
 // console.log(typeof startGame)
 
-startGameBtn.addEventListener('click', start);
+startGameBtn.addEventListener('click', function(){
+    console.log('Game is starting...', age);
+});
