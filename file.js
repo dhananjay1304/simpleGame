eed93c3475bce70d276a7101636b1df22cd47e8d
dhananjay1304 <dhananjaypotinda2003@gmail.com>
2023-08-15 22:17:07 +0530

    default arguments

--- file.js
+++ file.js
@@ -17,7 +17,7 @@ const getPlayerChoice = function () {
   ).toUpperCase();
   if (selection !== ROCK && selection !== PAPER && selection !== SCISSORS) {
     alert(`Invalid choice! We choose ${DEFAULT_USER_CHOICE} for you!`);
-    return DEFAULT_USER_CHOICE;
+    return;
   }
   return selection;
 };
@@ -33,7 +33,7 @@ const getComputerChoice = function () {
   }
 };
 
-const getWinner = (cChoice, pChoice) => {
+const getWinner = (cChoice, pChoice = DEFAULT_USER_CHOICE) => {
   cChoice === pChoice
     ? RESULT_DRAW
     : (cChoice === ROCK && pChoice === PAPER) ||
@@ -55,14 +55,29 @@ const getWinner = (cChoice, pChoice) => {
   //   }
 };
 
-startGameBtn.addEventListener("click", function () {
+startGameBtn.addEventListener("click", () => {
   if (gameIsRunning) {
     return;
   }
   gameIsRunning = true;
   console.log("Game is starting...");
-  const playersSelection = getPlayerChoice();
+  const playerChoice = getPlayerChoice();
   const computerChoice = getComputerChoice();
-  const winner = getWinner(computerChoice, playersSelection);
-  console.log(winner);
+  let winner;
+  if (playerChoice) {
+      winner = getWinner(computerChoice, playerChoice);
+    } else {
+      winner = getWinner(computerChoice, playerChoice);
+    }
+
+  let message = `you picked ${playerChoice || DEFAULT_USER_CHOICE}, computer picked ${computerChoice}, therefor you `;
+  if (winner === RESULT_DRAW) {
+    message = message + 'had a draw';
+  }else if (winner === RESULT_PLAYER_WINS) {
+    message = message + 'won';
+  }else {
+    message = message + 'lost';
+  }
+  alert(message);
+  gameIsRunning = false;
 });
