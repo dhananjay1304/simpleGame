b0094c5bae706ad8c6bfb8661aaa773401b2ccf8
dhananjay1304 <dhananjaypotinda2003@gmail.com>
2023-08-15 23:08:03 +0530

    no related to game code

--- file.js
+++ file.js
@@ -33,7 +33,7 @@ const getComputerChoice = function () {
   }
 };
 
-const getWinner = (cChoice, pChoice = cChoice ==='ROCK' ? PAPER: DEFAULT_USER_CHOICE) => {
+const getWinner = (cChoice, pChoice = DEFAULT_USER_CHOICE) => {
   cChoice === pChoice
     ? RESULT_DRAW
     : (cChoice === ROCK && pChoice === PAPER) ||
@@ -81,3 +81,25 @@ startGameBtn.addEventListener("click", () => {
   alert(message);
   gameIsRunning = false;
 });
+
+// Not releted to game
+
+const sumUp = (a, b, ...numbers) => {
+    let sum = 0;
+    for(const num of numbers) {
+        sum += num;
+    }
+    return sum;
+};
+
+const subtractUp = function() {
+    let sum = 0;
+    for (const num of arguments ) { // don't use that 
+        sum -= num;
+    }
+    return sum;
+}
+
+console.log(sumUp(0,15,20,50,-10,50));
+console.log (sumUp(1,5,-3,6,10,25,88));
+console.log(subtractUp(50,200));
